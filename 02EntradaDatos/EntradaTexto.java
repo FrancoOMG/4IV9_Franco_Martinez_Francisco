24ff00740d6b5823cfcb11887687f107540aaca9
FrancoOMG <ffrancom2400@alumno.ipn.mx>
2026-03-03 22:56:15 -0600

    Entrada De Texto

---
+++ 02EntradaDatos/EntradaTexto.java
@@ -0,0 +1,20 @@
+/* Vamos a crear una instancia y l asalida de datos estandar y la salida de datos*/
+import java.util.Scanner;
+
+class entradatexto{
+    //metodos principal
+    public static void main (String [] args){
+        Scanner entrada = new
+        //variables locales
+        String nombre;
+        int edad;
+        //entrada
+        System.out.println("Escribe tu nombre: ");
+        System.console().readLine();
+        System.out.println("Hola " + nombre + "benvenido a este programa muy feo")
+
+        System.out.println("Escribe tu edad: ")
+        edad = entrada.nextInt();
+        
+    }
+}
